c7f80ec1a940c106d6704cabc48429d266e5adac
Amij <amij86226680@gmail.com>
2024-04-12 16:22:01 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,217 @@
+console.log("test");
+var x = "Hi Class";
+console.log(x);
+console.log(x);
+let employeeName = "abebe";
+console.log(employeeName);
+
+// const laptopPrice = 2000;
+// console.log(laptopPrice);
+
+// // let discount = (laptopPrice * 10) / 100;
+// // let tax = (laptopPrice * 3) / 100;
+// // let finalPrice = laptopPrice - discount + tax;
+// // console.log(finalPrice);
+
+// var x = 10;
+// var y = 5;
+// var z = x + y;
+// console.log(z);
+// var z = x - y;
+// console.log(z);
+// var z = x * y;
+// console.log(z);
+// var z = x / y;
+// console.log(z);
+
+// // remainder
+
+// var x = 10;
+// var y = 4;
+// var z = x % y;
+// console.log(z);
+// //incriment operator(++)
+// var x = 3;
+// // x = x + 1;
+// x++;
+// console.log(x);
+// //decrement
+// var y = 3;
+// // y = y - 1;
+// y--;
+// console.log(y);
+// var x = 10;
+// var y = 4;
+// x -= y;
+// console.log(x);
+// let a = "hello";
+// let b = "world";
+// let c = a + " " + b;
+// console.log(c);
+
+// console.log(!false);
+
+// let num1 = 80;
+// let num2 = 15;
+// let sum;
+// let product;
+// let division;
+
+// console.log(num1);
+
+sum = num1 + num2;
+console.log(sum);
+
+// product = num1 * num2;
+// console.log(product);
+
+// function add() {
+//   console.log(43 + 32);
+//   console.log("hi");
+// }
+// add();
+// let addition = function () {
+//   console.log("hi from addition function");
+//   console.log(10 + 7);
+// };
+// addition();
+
+// function addTwoNumbers(a, b) {
+//   console.log(a + b);
+
+//   //   console.log(a * b);
+//   //   console.log(a - b);
+// }
+
+// addTwoNumbers(5, 9);
+
+// addTwoNumbers(12, 8);
+// addTwoNumbers("amir", "jemal");
+
+// // return
+// function adder(x, y) {
+//   let c = x + y;
+//   console.log(c);
+//   return c;
+// }
+
+// function average(w, t) {
+//   let result = adder(w, t) / 2;
+//   console.log(result);
+//   //   return result
+// }
+// average(100, 20);
+
+// // Arrow functions
+
+// // normal function declaration
+// function addTwoNumbers1(num1, num2) {
+//   return num1 + num2;
+// }
+// console.log(addTwoNumbers1(6, 4));
+
+// //function expression
+// const addTwoNumbers2 = function (num1, num2) {
+//   return num1 + num2;
+// };
+// console.log(addTwoNumbers1(7, 9));
+
+// //arrow function
+// const addTwoNumbers3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(addTwoNumbers3(16, 20));
+
+// // if statement
+// if (true) {
+//   console.log("Amiruaa");
+// }
+// // function with if statement
+// function checkIfPassed(pass, score, name) {
+//   if (score >= pass) {
+//     console.log(name + ":passed");
+//   }
+//   if (score < pass) {
+//     console.log(name + ":failed");
+//   }
+// }
+// checkIfPassed(50, 38, "abebe");
+// checkIfPassed(50, 96, "kebe");
+// checkIfPassed(50, 40, "jams");
+// checkIfPassed(50, 75, "haz");
+
+// // if else statement
+
+// // ex.1
+// function PasswordChecker(pass1, pass2) {
+//   if (pass1 === pass2) {
+//     console.log("successfully logged in");
+//   } else {
+//     console.log("wrong password");
+//   }
+// }
+// PasswordChecker("test123", "test123");
+// PasswordChecker("abebe678", "test123");
+
+// // ex.2
+// function compareNumbers(x, y) {
+//   if (x > y) {
+//     console.log(x + " is greater from " + y);
+//   } else if (x === y) {
+//     console.log(x + " is equal to " + y);
+//   } else {
+//     console.log(x + " is less than " + y);
+//   }
+// }
+
+// compareNumbers(30, 25);
+
+// compareNumbers(78, 90);
+
+// compareNumbers(42, 42);
+
+// // turnery operator
+// function PasswordChecker(pass1, pass2) {
+//   pass1 === pass2
+//     ? console.log("successfully logged in")
+//     : console.log("wrong password");
+// }
+
+// PasswordChecker("test", "test");
+// PasswordChecker("abebe", "test");
+
+// // switch
+// function whatToDrink(time) {
+//   let drink;
+//   switch (time) {
+//     case "morning":
+//       drink = "Tea";
+//       break;
+//     case "afternoon":
+//       drink = "coffee";
+//       break;
+//     default:
+//       drink = "water";
+//   }
+//   return drink;
+// }
+// console.log(whatToDrink("morning"));
+// console.log(whatToDrink("afternoon"));
+// console.log(whatToDrink("night"));
+// console.log(whatToDrink("daytime"));
+
+// // Arrays
+
+// let fruits = ["Apple", "banana", "Mango", "Orange", "Papaya"];
+// console.log(fruits);
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+// console.log(fruits[4]);
+
+// // changing array element
+
+// fruits[1] = "grapes";
+// console.log(fruits);
